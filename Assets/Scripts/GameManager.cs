9263d05f15deca59861d8df97ba5dc277b5b69b5
Christian Hernandez <teckiealwayz@gmail.com>
2025-12-12 17:14:47 -0600

    Moved some functions back to ObjectHandler also added win state

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,17 +1,35 @@
 using System.Collections.Generic;
+using System;
 using System.Collections;
 using UnityEngine;
 using System.Linq;
 using System.ComponentModel.Design.Serialization;
+using Unity.VisualScripting;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
+    public GameState State;
+
+    public static event Action<GameState> OnGameStateChange;
     public List<GameObject> insideCups;
     public List<GameObject> outsideCups;
 
     public Transform insideParent;
 
     public List<GameObject> clickedOn = new();
+
+        readonly List<(float x,float y)> pos = new();
+
+    // Fisher-Yates shuffle algorithm
+    public static void Shuffle<T>(List<T> list)
+    {
+        int n = list.Count;
+        for (int i = n - 1; i > 0; i--)
+        {
+            int j = UnityEngine.Random.Range(0, i + 1);
+            (list[j], list[i]) = (list[i], list[j]);
+        }
+    }
 
 
 
@@ -30,7 +48,13 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
-
+        Shuffle(insideCups);
+        pos.Add((-3f,-2.5f));
+        pos.Add((-1f,-2.5f));
+        pos.Add((1f,-2.5f));
+        pos.Add((3f,-2.5f));
+        ScrambleSetInsideCups();
+        UpdateGameState(GameState.Trying);
     }
 
     void Update()
@@ -38,7 +62,37 @@ public class GameManager : MonoBehaviour
 
     }
 
+    void ScrambleSetInsideCups()
+{
+    int i = 0;
+    foreach (GameObject x in GameManager.Instance.insideCups)
+    {
+        Instantiate(x, new Vector3(pos[i].x,pos[i].y), Quaternion.identity, GameManager.Instance.insideParent);
+        i++;
+    }
+}
+
+    public void UpdateGameState(GameState newState)
+    {
+        State = newState;
+
+        switch(newState)
+        {
+            case GameState.Trying:
+                break;
+            case GameState.Win:
+                break;
+        }
+    
+    OnGameStateChange?.Invoke(newState);
+
+    }
+
+}
 
 
-
+public enum GameState
+{
+    Trying,
+    Win,
 }
--- Assets/Scripts/ObjectHandler.cs
+++ Assets/Scripts/ObjectHandler.cs
@@ -5,18 +5,7 @@ using UnityEngine.EventSystems;
 
 public class ObjectHandler : MonoBehaviour, IPointerClickHandler
 {
-    readonly List<(float x,float y)> pos = new();
 
-    // Fisher-Yates shuffle algorithm
-    public static void Shuffle<T>(List<T> list)
-    {
-        int n = list.Count;
-        for (int i = n - 1; i > 0; i--)
-        {
-            int j = Random.Range(0, i + 1);
-            (list[j], list[i]) = (list[i], list[j]);
-        }
-    }
     public void OnPointerClick(PointerEventData eventData)
     {
         if (GameManager.Instance.clickedOn.Count == 0)
@@ -44,74 +33,61 @@ public class ObjectHandler : MonoBehaviour, IPointerClickHandler
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+void Start()
+{
+
+}
+
+
+
+    public void Swap()
+{
+    // use this as midpoint to create pivot point to revolve objects
+    var objDistance = (GameManager.Instance.clickedOn[0].transform.position + GameManager.Instance.clickedOn[1].transform.position) / 2;
+    StartCoroutine(RotateTimed(0.5f, GameManager.Instance.clickedOn[0], objDistance));
+    StartCoroutine(RotateTimed(.5f, GameManager.Instance.clickedOn[1], objDistance));
+    SwapListPos();
+    GameManager.Instance.clickedOn.Clear();
+}
+
+IEnumerator RotateTimed(float duration, GameObject clicked, Vector3 objDistance)
+{
+    float timer = 0f;
+    float startAngle = 0f;
+    float currentAngle;
+    while (timer < duration)
     {
-        Shuffle(GameManager.Instance. insideCups);
-        pos.Add((-3f,-2.5f));
-        pos.Add((-1f,-2.5f));
-        pos.Add((1f,-2.5f));
-        pos.Add((3f,-2.5f));
-        ScrambleSetInsideCups();
+        // clicked.transform.RotateAround(objDistance, Vector3.up, 1440 * Time.deltaTime);
+        timer += Time.deltaTime;
+
+        // Lerp rotation progress
+        currentAngle = Mathf.Lerp(startAngle, 180f, timer / duration);
+
+        // Rotate difference since last frame
+        clicked.transform.RotateAround(objDistance, Vector3.up, currentAngle - startAngle);
+
+        startAngle = currentAngle;
+
+        yield return null;
     }
+    // set cups back down
+    clicked.transform.position = new Vector3(clicked.transform.position.x, clicked.transform.position.y - 0.25f);
+}
 
-        void ScrambleSetInsideCups()
-    {
-        int i = 0;
-        foreach (GameObject x in GameManager.Instance.insideCups)
-        {
-            Instantiate(x, new Vector3(pos[i].x,pos[i].y), Quaternion.identity, GameManager.Instance.insideParent);
-            i++;
-        }
-    }
+public void SwapListPos()
+{
+    // find index of items that are clicked on to be swapped
+    int indexA = GameManager.Instance.insideCups.FindIndex(c => c.name == GameManager.Instance.clickedOn[0].name);
+    int indexB = GameManager.Instance.insideCups.FindIndex(c => c.name == GameManager.Instance.clickedOn[1].name);
 
-        public void Swap()
-    {
-        // use this as midpoint to create pivot point to revolve objects
-        var objDistance = (GameManager.Instance.clickedOn[0].transform.position + GameManager.Instance.clickedOn[1].transform.position) / 2;
-        StartCoroutine(RotateTimed(0.5f, GameManager.Instance.clickedOn[0], objDistance));
-        StartCoroutine(RotateTimed(.5f, GameManager.Instance.clickedOn[1], objDistance));
-        SwapListPos();
-        GameManager.Instance.clickedOn.Clear();
-    }
+    // swap items in the list to then be compared
+    (GameManager.Instance.insideCups[indexB], GameManager.Instance.insideCups[indexA]) = (GameManager.Instance.insideCups[indexA], GameManager.Instance.insideCups[indexB]);
+}
 
-    IEnumerator RotateTimed(float duration, GameObject clicked, Vector3 objDistance)
-    {
-        float timer = 0f;
-        float startAngle = 0f;
-        float currentAngle;
-        while (timer < duration)
-        {
-            // clicked.transform.RotateAround(objDistance, Vector3.up, 1440 * Time.deltaTime);
-            timer += Time.deltaTime;
-
-            // Lerp rotation progress
-            currentAngle = Mathf.Lerp(startAngle, 180f, timer / duration);
-
-            // Rotate difference since last frame
-            clicked.transform.RotateAround(objDistance, Vector3.up, currentAngle - startAngle);
-
-            startAngle = currentAngle;
-
-            yield return null;
-        }
-        // set cups back down
-        clicked.transform.position = new Vector3(clicked.transform.position.x, clicked.transform.position.y - 0.25f);
-    }
-
-    public void SwapListPos()
-    {
-        // find index of items that are clicked on to be swapped
-        int indexA = GameManager.Instance.insideCups.FindIndex(c => c.name == GameManager.Instance.clickedOn[0].name);
-        int indexB = GameManager.Instance.insideCups.FindIndex(c => c.name == GameManager.Instance.clickedOn[1].name);
-
-        // swap items in the list to then be compared
-        (GameManager.Instance.insideCups[indexB], GameManager.Instance.insideCups[indexA]) = (GameManager.Instance.insideCups[indexA], GameManager.Instance.insideCups[indexB]);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+// Update is called once per frame
+void Update()
+{
+    
+}
 
 }
